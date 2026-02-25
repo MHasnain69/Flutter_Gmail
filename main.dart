99c1d9bb74dc94415f9ea38b65beb61e68ac62d2
MHasnain <152295215+MHasnain69@users.noreply.github.com>
2026-02-26 02:16:49 +0500

    Refactor Gmail UI code structure

--- main.dart
+++ main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// Muhammad Hasnain
 
 void main() {
   runApp(const GmailUI());
@@ -134,3 +135,4 @@ class EmailTile extends StatelessWidget {
     );
   }
 }
+
